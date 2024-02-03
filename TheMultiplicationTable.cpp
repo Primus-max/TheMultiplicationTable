126ae90d8cb6b793e795d438ea46528bd49fb923
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-02-03 21:00:30 +0300

    Добавьте файлы проекта.

---
+++ TheMultiplicationTable.cpp
@@ -0,0 +1,22 @@
+﻿// Написать программу, которая выводит на экран таблицу
+// умножения на k, где k — номер варианта.Например, для 7 - го  варианта 
+
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int userInputK = 0, border = 10;
+
+    cout << "Для вывода таблицы умножения введите k: ";
+    cin >> userInputK;
+
+    for (int i = 0; i <= border; i++)    
+        cout << userInputK << " * " << i << " = " << userInputK * i << endl;
+   
+}
+
